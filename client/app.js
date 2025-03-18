064160e9e94b8eb5f4557a62b4a7f04776316594
Jan <janogiegl@gmail.com>
2025-03-18 07:30:00 +0100

    drawing works

--- client/app.js
+++ client/app.js
@@ -1,51 +1,14 @@
 const canvas = document.getElementById('drawing-board');
-const toolbar = document.getElementById('toolbar');
 const ctx = canvas.getContext('2d');
 
-const canvasOffsetX = canvas.offsetLeft;
-const canvasOffsetY = canvas.offsetTop;
-
-canvas.width = window.innerWidth - canvasOffsetX;
-canvas.height = window.innerHeight - canvasOffsetY;
-
-let isPainting = false;
-let lineWidth = 5;
-let startX;
-let startY;
-
-toolbar.addEventListener('click', e => {
-    if (e.target.id === 'clear') {
-        ctx.clearRect(0, 0, canvas.width, canvas.height);
-    }
-});
-
-toolbar.addEventListener('change', e => {
-    if(e.target.id === 'stroke') {
-        ctx.strokeStyle = e.target.value;
-    }
-
-    if(e.target.id === 'lineWidth') {
-        lineWidth = e.target.value;
-    }
-    
-});
-
-const draw = (e) => {
-    if(!isPainting) {
-        return;
-    }
-
-    ctx.lineWidth = lineWidth;
-    ctx.lineCap = 'round';
-
-    ctx.lineTo(e.clientX - canvasOffsetX, e.clientY);
-    ctx.stroke();
-}
+let isDrawing = false;
+const socket = new WebSocket("ws://localhost:8000/ws");
 
 canvas.addEventListener('mousedown', (e) => {
     isPainting = true;
     startX = e.clientX;
     startY = e.clientY;
+    draw();
 });
 
 canvas.addEventListener('mouseup', e => {
@@ -53,5 +16,46 @@ canvas.addEventListener('mouseup', e => {
     ctx.stroke();
     ctx.beginPath();
 });
-
 canvas.addEventListener('mousemove', draw);
+
+const canvasOffsetX = canvas.offsetLeft;
+const canvasOffsetY = canvas.offsetTop;
+
+canvas.width = window.innerWidth - canvasOffsetX;
+canvas.height = window.innerHeight - canvasOffsetY;
+
+function draw(e) {
+    if (!isDrawing) return;
+
+    const x = e.clientX;
+    const y = e.clientY;
+    const strokeStyle = document.getElementById('stroke').value;
+    const lineWidth = document.getElementById('lineWidth').value;
+
+    ctx.lineWidth = lineWidth;
+    ctx.strokeStyle = strokeStyle;
+    ctx.lineCap = 'round';
+
+    ctx.lineTo(e.clientX - canvasOffsetX, e.clientY);
+    ctx.stroke();
+    
+    //ctx.moveTo(x, y);
+
+    // Send drawing data to the server
+    socket.send(JSON.stringify({ x, y, strokeStyle, lineWidth }));  
+
+}
+
+// Receive drawing data from other clients
+socket.onmessage = (event) => {
+    const data = JSON.parse(event.data);
+
+    ctx.lineWidth = data.lineWidth;
+    ctx.strokeStyle = data.strokeStyle;
+    ctx.lineCap = 'round';
+
+    ctx.lineTo(data.x, data.y);
+    ctx.stroke();
+    ctx.beginPath();
+    ctx.moveTo(data.x, data.y);
+};
